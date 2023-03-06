257ae39e03c8421d508a73388668e022b3c8666e
Sadman26 <fareless.ranger@gmail.com>
2023-03-06 22:40:11 +0600

    initialized db

--- app.py
+++ app.py
@@ -22,3 +22,14 @@ import os
 from pathlib import Path
 import png
 from datetime import datetime
+#Weather Api_Key
+api_key = '30d4741c779ba94c470ca1f63045390a'
+#Wiki Api Key
+WIKI_REQUEST = 'http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='
+client=MongoClient('localhost',27017)
+db=client['keep']
+loginx=db['login']
+notex=db['note']
+reminder=db['reminder']
+app = Flask(__name__,template_folder='temp')
+app.secret_key='secret'
