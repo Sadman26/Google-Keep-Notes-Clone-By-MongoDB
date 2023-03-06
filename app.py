f6b1ea6d733297304ab3d300924649276c7a4a81
Sadman26 <fareless.ranger@gmail.com>
2023-03-06 22:37:41 +0600

    Added all the modules needed

---
+++ app.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import cv2
+from flask import *
+from twilio.rest import Client
+##Text to speech
+import pyttsx3
+from pyqrcode import QRCode
+import speech_recognition as sr
+import threading
+import json
+import plyer
+import requests
+#!For time related 
+import sched
+import time as time_module
+import random
+import wikipedia
+import qrcode
+import string
+from pytube import YouTube
+import os
+from pathlib import Path
+import png
+from datetime import datetime
